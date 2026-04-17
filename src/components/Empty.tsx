25e9048fff67fb53daac89f8355d27470458df68
iliarocks <parun.ilia@gmail.com>
2026-04-16 19:08:48 -0700

    refactor: changed empty state message

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 const Empty: Component = () => {
 	return (
 		<div class="grid place-items-center h-full w-full text-[var(--secondary)]">
-			<h1>No tasks... for now</h1>
+			<h1>All clear</h1>
 		</div>
 	);
 };
